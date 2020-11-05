829272a4b44ee8ef450ccd757210e2763f3eece3
Bill Xiong <xiong.bill@yahoo.com>
2020-11-04 21:44:06 -0800

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,26 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+describe('no errors for validation for an input that is more than 3 long', () => {
+  test('fills out form and submits without error', () => {
+    const utils = render(<App />);
+    const firstNameInput = screen.getByPlaceholderText('Bill');
+    const lastNameInput = screen.getByPlaceholderText(/Xiong/i);
+    const email = screen.getByPlaceholderText(/xiong.bill@yahoo.com/i);
+    fireEvent.change(firstNameInput, { target: {value: 'Elon Musk'}});
+    fireEvent.change(lastNameInput, { target: {value: 'Gates'}});
+    fireEvent.change(email, { target: {value: 'Gates'}});
+    expect(firstNameInput.value).toBe('Elon Musk');
+    expect(lastNameInput.value).toBe('Gates');
+    expect(email.value).toBe('Gates');
+
+    const button = screen.getByRole("button", {name: /submit/i});
+    fireEvent.click(button);
+
+  })
+})
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,8 +17,9 @@ const ContactForm = () => {
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
-            placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            placeholder="Bill"
+            ref={register({ required: true
+            })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -29,7 +30,7 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
-            placeholder="Burke"
+            placeholder="Xiong"
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -38,10 +39,10 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label htmlFor="email" placeholder="xiong.bill@yahoo.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" placeholder="xiong.bill@yahoo.com" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
